4aea8d7190842e0e5a947c65ab74d1eda757e505
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-03-11 12:12:43 -0400

    added a few notes

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const button = document.querySelector('button');
 const input = document.getElementById('item');
+//use this underneath for local storage
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
 
+//use this underneath for local storage
 localStorage.setItem('items', JSON.stringify(itemsArray));
 const data = JSON.parse(localStorage.getItem('items'));
 
@@ -19,6 +21,7 @@ const liMaker = (text) => {
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
+  //use this underneath for local storage
   itemsArray.push(input.value);
   localStorage.setItem('items', JSON.stringify(itemsArray));
 
@@ -32,7 +35,7 @@ data.forEach(item => {
 });
 
 button.addEventListener('click', function () {
-  localStorage.clear();
+  localStorage.clear(); //clears list and local storage
   while (ul.firstChild) {
     ul.removeChild(ul.firstChild);
   }
